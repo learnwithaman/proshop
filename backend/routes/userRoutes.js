a5aea3c991fe5d6b20c11a7221a86aa9d750af3e
learnwithaman <learnwithaman@gmail.com>
2021-03-26 17:56:14 +0530

    Add delete user feature

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/', registerUser);
-router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/:id').delete(protect, admin, deleteUser)
 
-export default router;
+export default router
